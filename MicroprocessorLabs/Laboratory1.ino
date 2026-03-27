ad71554a9729b6f2870647c1d0feaff0ffc6f0c6
Axel Gesmundo <97203517+White-Lotus-id@users.noreply.github.com>
2026-03-28 00:06:42 +0800

    Add files via upload

---
+++ MicroprocessorLabs/Laboratory1.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // set pin 13 as an Output
+  delay(1000); //Wait 1 second
+  digitalWrite(13, LOW); //turn led off
+  delay(1000); //wait 1 second
+}
